9d3c27faa866e023ea35ad79a3a415df3038e416
yetison <46581058+yetison@users.noreply.github.com>
2026-05-06 15:24:55 +0200

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,8 @@
+{
+  module: "MMM-HuaweiSolar",
+  position: "top_right", // Du kannst auch "top_left", "bottom_right" etc. nutzen
+  header: "Elektrizität",
+  config: {
+    updateInterval: 5000 // Aktualisierung alle 5 Sekunden (analog zum ESP)
+  }
+},
